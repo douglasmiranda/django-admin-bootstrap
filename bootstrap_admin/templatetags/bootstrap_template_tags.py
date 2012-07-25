6285ebdc8f6af97181cfd897ae7453e49098fffb
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-07-25 02:50:41 -0400

    A simples customization on change_form.
    I'll make more changes, after test with more widgets.

---
+++ bootstrap_admin/templatetags/bootstrap_template_tags.py
@@ -0,0 +1,75 @@
+"""
+These functions bellow are from django-widget-tweaks.
+(silence_without_field, _process_field_attributes, append_attr, add_class, widget_type)
+
+By Mikhail Korobov and contributors, more information:
+Copyright (c) 2011 Mikhail Korobov
+LICENSE: https://github.com/kmike/django-widget-tweaks/blob/master/LICENSE
+"""
+
+import re
+from django.template import Library
+register = Library()
+
+
+def silence_without_field(fn):
+    def wrapped(field, attr):
+        if not field:
+            return ""
+        else:
+            return fn(field, attr)
+    return wrapped
+
+
+def _process_field_attributes(field, attr, process):
+
+    # split attribute name and value from 'attr:value' string
+    params = attr.split(':', 1)
+    attribute = params[0]
+    value = params[1] if len(params) == 2 else ''
+
+    # decorate field.as_widget method with updated attributes
+    old_as_widget = field.as_widget
+
+    def as_widget(self, widget=None, attrs=None, only_initial=False):
+        attrs = attrs or {}
+        process(widget or self.field.widget, attrs, attribute, value)
+        return old_as_widget(widget, attrs, only_initial)
+
+    bound_method = type(old_as_widget)
+    try:
+        field.as_widget = bound_method(as_widget, field, field.__class__)
+    except TypeError: # python 3
+        field.as_widget = bound_method(as_widget, field)
+    return field
+
+
+@register.filter("append_attr")
+@silence_without_field
+def append_attr(field, attr):
+    def process(widget, attrs, attribute, value):
+        if attrs.get(attribute):
+            attrs[attribute] += ' ' + value
+        elif widget.attrs.get(attribute):
+            attrs[attribute] = widget.attrs[attribute] + ' ' + value
+        else:
+            attrs[attribute] = value
+    return _process_field_attributes(field, attr, process)
+
+
+@register.filter("add_class")
+@silence_without_field
+def add_class(field, css_class):
+    return append_attr(field, 'class:' + css_class)
+
+
+@register.filter(name='widget_type')
+def widget_type(field):
+    """
+    Template filter that returns field widget class name (in lower case).
+    E.g. if field's widget is TextInput then {{ field|widget_type }} will
+    return 'textinput'.
+    """
+    if hasattr(field, 'field') and hasattr(field.field, 'widget') and field.field.widget:
+        return field.field.widget.__class__.__name__.lower()
+    return ''
