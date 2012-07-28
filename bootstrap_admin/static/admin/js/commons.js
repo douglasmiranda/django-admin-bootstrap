53a8f95bb819e97f8f987d3eac1d069638e6a898
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-07-28 13:39:35 -0400

    Changes on load jquery:
    new jquery file jquery.min load in production 1.7.2
    jquery local not minified for debug
    adding the "django.jQuery" in the templates and js files as needed to preserve compatibility

--- bootstrap_admin/static/admin/js/commons.js
+++ bootstrap_admin/static/admin/js/commons.js
@@ -1,6 +1,8 @@
-$.fn.isAfter = function(sel){
-    return this.prevAll(sel).length !== 0;
-}
-$.fn.isBefore= function(sel){
-    return this.nextAll(sel).length !== 0;
-}
+(function($) {
+    $.fn.isAfter = function(sel){
+        return this.prevAll(sel).length !== 0;
+    }
+    $.fn.isBefore= function(sel){
+        return this.nextAll(sel).length !== 0;
+    }
+})(django.jQuery);
