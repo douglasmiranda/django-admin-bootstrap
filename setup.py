539d9508522cb82336d1fd63300c0da0529ec18a
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-12-20 00:47:09 -0300

    setup ready to pypi register

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
+from bootstrap_admin import VERSION
 
-
-template_dirs = [
-    'templates/admin/auth/user/*.html',
-    'templates/admin/edit_inline/*.html',
-    'templates/admin/includes/*.html',
-    'templates/admin/*.html',
-    'templates/registration/*.html',
-    'static/admin/css/*.css',
-    'static/admin/js/*.js',
-    'static/admin/js/admin/*.js',
-    'static/admin/img/*.png',
-    'templatetags/*',
-]
+f = open(os.path.join(os.path.dirname(__file__), 'README.markdown'))
+readme = f.read()
+f.close()
 
 setup(
     name='bootstrap_admin',
-    version='0.1',
-    description='Responsive Templates para o Django Admin, customizados com Twitter Bootstrap.',
+    version=".".join(map(str, VERSION)),
+    description='Twitter Bootstrap Responsive Skin for Django Admin.',
+    long_description=readme,
     author='Douglas Miranda',
     author_email='douglasmirandasilva@gmail.com',
     url='https://github.com/douglasmiranda/django-admin-bootstrap',
     packages=find_packages(),
-    package_data={'bootstrap_admin': template_dirs},
-    license='GPL',
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
 )
