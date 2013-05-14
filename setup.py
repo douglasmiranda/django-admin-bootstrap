f433ba55800a75ef2e714f52c9e4db4fe6d77e2e
Douglas Miranda <douglasmirandasilva@gmail.com>
2013-05-14 01:07:20 -0400

    setup: bump to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ f.close()
 
 setup(
     name='bootstrap_admin',
-    version='0.1.9',
+    version='0.2.0',
     description='Twitter Bootstrap Responsive Skin for Django Admin.',
     long_description=readme,
     author='Douglas Miranda',
