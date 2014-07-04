2a6aa468c6f610b1ba798dcdfeecb889077946bb
Douglas Miranda <douglasmirandasilva@gmail.com>
2014-07-04 03:11:11 -0400

    Prep for #39 and #43

--- bootstrap_admin/static/admin/js/commons.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $.fn.isAfter = function(sel){
-        return this.prevAll(sel).length !== 0;
-    }
-    $.fn.isBefore= function(sel){
-        return this.nextAll(sel).length !== 0;
-    }
-})(django.jQuery);
