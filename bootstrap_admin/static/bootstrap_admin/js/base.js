88ee8d1934eecbdccd591c2658bcf87046a84ad3
Douglas Miranda <douglasmirandasilva@gmail.com>
2014-07-24 21:00:03 -0400

    Base html/css/js, new sidebar menu and global apps listing.

---
+++ bootstrap_admin/static/bootstrap_admin/js/base.js
@@ -0,0 +1,63 @@
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014 Twitter, Inc.
+ * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+ * details, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+// See the Getting Started docs for more information:
+// http://getbootstrap.com/getting-started/#support-ie10-width
+
+(function () {
+  'use strict';
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.querySelector('head').appendChild(msViewportStyle)
+  }
+})();
+
+/*! jquery.cookie v1.4.1 | MIT */
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?a(require("jquery")):a(django.jQuery)}(function(a){function b(a){return h.raw?a:encodeURIComponent(a)}function c(a){return h.raw?a:decodeURIComponent(a)}function d(a){return b(h.json?JSON.stringify(a):String(a))}function e(a){0===a.indexOf('"')&&(a=a.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\"));try{return a=decodeURIComponent(a.replace(g," ")),h.json?JSON.parse(a):a}catch(b){}}function f(b,c){var d=h.raw?b:e(b);return a.isFunction(c)?c(d):d}var g=/\+/g,h=a.cookie=function(e,g,i){if(void 0!==g&&!a.isFunction(g)){if(i=a.extend({},h.defaults,i),"number"==typeof i.expires){var j=i.expires,k=i.expires=new Date;k.setTime(+k+864e5*j)}return document.cookie=[b(e),"=",d(g),i.expires?"; expires="+i.expires.toUTCString():"",i.path?"; path="+i.path:"",i.domain?"; domain="+i.domain:"",i.secure?"; secure":""].join("")}for(var l=e?void 0:{},m=document.cookie?document.cookie.split("; "):[],n=0,o=m.length;o>n;n++){var p=m[n].split("="),q=c(p.shift()),r=p.join("=");if(e&&e===q){l=f(r,g);break}e||void 0===(r=f(r))||(l[q]=r)}return l};h.defaults={},a.removeCookie=function(b,c){return void 0===a.cookie(b)?!1:(a.cookie(b,"",a.extend({},c,{expires:-1})),!a.cookie(b))}});
+
+(function ($) {
+  $(document).ready(function(){
+    $("#overlay").delay(600).fadeOut(500);
+
+    var show_hide_sidebar_menu = function () {
+      hidden_menu = $('#sidebar-menu').data('hidden');
+      main_classes = 'col-xs-9 col-xs-offset-6 col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2';
+      if (hidden_menu) {
+        $('.main').addClass(main_classes).removeClass('col-sm-12');
+        $('.sidebar-menu').css('left', '0').data('hidden', false);
+        $.removeCookie('hidden_sidebar_menu');
+      } else {
+        $('.main').removeClass(main_classes).addClass('col-sm-12');
+        $('.sidebar-menu').css('left', '-50%').data('hidden', true);
+        $.cookie('hidden_sidebar_menu', true);
+      }
+    };
+    if ($.cookie('hidden_sidebar_menu')) {
+      show_hide_sidebar_menu();
+    }
+    $('.show-hide-sidebar-menu').on('click', function(e){
+      show_hide_sidebar_menu();
+      e.preventDefault();
+    });
+
+    // list models
+    $('.nav-sidebar li:first-child a').on('click', function () {
+      var nav_sidebar = $(this).closest('.nav-sidebar');
+      if (nav_sidebar.data('status') == 'open') {
+        nav_sidebar.removeClass('show-models').data('status', 'closed');
+      } else {
+        nav_sidebar.addClass('show-models').data('status', 'open');
+      }
+      return false;
+    });
+  });
+})(django.jQuery);
--- bootstrap_admin/templatetags/bootstrap_admin_template_tags.py
+++ bootstrap_admin/templatetags/bootstrap_admin_template_tags.py
@@ -1,3 +1,10 @@
+from django.contrib.admin.sites import site
+from django.apps import apps
+from django.utils.text import capfirst
+from django.core.urlresolvers import reverse, NoReverseMatch
+from django.core.exceptions import ImproperlyConfigured
+from django.utils import six
+from django.conf import settings
 from django import template
 register = template.Library()
 
@@ -25,3 +32,77 @@ def add_class(field, custom_classes=''):
 def placeholder(field, placeholder=''):
     field.field.widget.attrs.update({'placeholder': placeholder})
     return field
+
+
+@register.inclusion_tag('bootstrap_admin/sidebar_menu.html', takes_context=True)
+def render_menu_app_list(context):
+    show_global_menu = getattr(settings, 'BOOTSTRAP_ADMIN_SIDEBAR_MENU', False)
+    dependencie = 'django.core.context_processors.request'
+    if not show_global_menu:
+        return {'app_list': ''}
+
+    if (dependencie not in settings.TEMPLATE_CONTEXT_PROCESSORS):
+        raise ImproperlyConfigured(
+            "bootstrap_admin: in order to use the 'sidebar menu' requires the \
+            '%s' to be added to settings.TEMPLATE_CONTEXT_PROCESSORS."
+            % dependencie
+        )
+
+    # Code adapted from django.contrib.admin.AdminSite
+    app_dict = {}
+    user = context.get('user')
+    for model, model_admin in site._registry.items():
+        app_label = model._meta.app_label
+        has_module_perms = user.has_module_perms(app_label)
+
+        if has_module_perms:
+            perms = model_admin.get_model_perms(context.get('request'))
+
+            # Check whether user has any perm for this module.
+            # If so, add the module to the model_list.
+            if True in perms.values():
+                info = (app_label, model._meta.model_name)
+                model_dict = {
+                    'name': capfirst(model._meta.verbose_name_plural),
+                    'object_name': model._meta.object_name,
+                    'perms': perms,
+                }
+                if perms.get('change', False):
+                    try:
+                        model_dict['admin_url'] = reverse(
+                            'admin:%s_%s_changelist' % info,
+                            current_app=site.name
+                        )
+                    except NoReverseMatch:
+                        pass
+                if perms.get('add', False):
+                    try:
+                        model_dict['add_url'] = reverse(
+                            'admin:%s_%s_add' % info,
+                            current_app=site.name
+                        )
+                    except NoReverseMatch:
+                        pass
+                if app_label in app_dict:
+                    app_dict[app_label]['models'].append(model_dict)
+                else:
+                    app_dict[app_label] = {
+                        'name': apps.get_app_config(app_label).verbose_name,
+                        'app_label': app_label,
+                        'app_url': reverse(
+                            'admin:app_list',
+                            kwargs={'app_label': app_label},
+                            current_app=site.name
+                        ),
+                        'has_module_perms': has_module_perms,
+                        'models': [model_dict],
+                    }
+
+    # Sort the apps alphabetically.
+    app_list = list(six.itervalues(app_dict))
+    app_list.sort(key=lambda x: x['name'].lower())
+
+    # Sort the models alphabetically within each sapp.
+    for app in app_list:
+        app['models'].sort(key=lambda x: x['name'])
+    return {'app_list': app_list}
