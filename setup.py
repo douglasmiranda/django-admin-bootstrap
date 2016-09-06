f166ef6c9e4b4463ae9ca1a3af653aef18b1a77c
Douglas Miranda <douglasmirandasilva@gmail.com>
2016-09-06 02:52:08 -0400

    setup: bump to 0.3.7.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ f.close()
 
 setup(
     name='bootstrap_admin',
-    version='0.3.7',
+    version='0.3.7.1',
     description='Responsive Theme for Django Admin (Django 1.7)',
     long_description=readme,
     author='Douglas Miranda',
