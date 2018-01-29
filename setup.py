a8fe25a86c2686c065289e6229d48326aedf5399
Douglas Miranda <douglasmirandasilva@gmail.com>
2018-01-28 21:58:02 -0300

    setup: bump to 0.3.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ f.close()
 
 setup(
     name='bootstrap_admin',
-    version='0.3.8',
+    version='0.3.9',
     description='Responsive Theme for Django Admin With Sidebar Menu (Django 1.11)',
     long_description=readme,
     author='Douglas Miranda',
